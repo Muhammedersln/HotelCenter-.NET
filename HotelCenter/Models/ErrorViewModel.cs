4bbd6aaffd4a1909eb91c4e019b81911dd1009c3
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-08-06 21:14:25 +0300

    Add project files.

---
+++ HotelCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
